ea3660489467cca912b786a248f9aa8260b65c27
Lakshmivenkateshwaran <vvenkivasudevan9@gmail.com>
2024-03-14 14:30:39 +0530

    Commited the code

---
+++ README.md
@@ -0,0 +1,35 @@
+# SCRAPY_DATA_PROJECT_APP
+
+This Scrapy project is designed to scrape recipes and related information from a verified website dedicated to culinary delights. The spider crawls through the website's pages, extracting recipe titles, ingredients, instructions, cooking times, and more. The extracted data is then stored in structured JSON format for further analysis and use.
+
+# Scraped Website: 'https://www.vegrecipesofindia.com/recipes/south-indian-recipes/'
+
+## Project Structure
+
+- scrapy.cfg            # Scrapy configuration file
+- myproject             # Scrapy project directory
+- spiders               # Directory for Scrapy spiders
+- __init__.py           # Python initialization file
+- receipes_spider.py    # Scrapy spider for scraping recipes
+- items.py              # Defines the data model for scraped items
+- middlewares.py        # Scrapy middleware settings
+- pipelines.py          # Pipeline for processing scraped items
+- settings.py           # Scrapy project settings
+- data_cleaning.py      # Python script for data cleaning with pandas
+- final_recipes.json    # Final json data which has some empty objects
+- updated_
+final_recipes.json      # Updated and cleaned Data
+- README.md             # Project documentation
+- requirements.txt      # List of project dependencies
+
+## Getting Started
+
+To get started with the project, follow these steps:
+
+1. Clone the repository.
+2. Navigate to the project directory.
+3. Set up a virtual environment (optional but recommended).
+   python -m venv env
+   env\Scripts\activate  # On Windows
+   source env/bin/activate  # On macOS/Linux
+4. Install the required dependencies using `pip install -r requirements.txt`.
---
+++ myproject/data_cleaning.py
@@ -0,0 +1,37 @@
+import json
+import pandas as pd
+
+# Read the JSON file into a pandas DataFrame
+df = pd.read_json(r"C:\Users\venki\Work\scrapy_data_project_app\myproject\final_recipes.json")
+
+
+if 'receipe_data' in df.columns:
+    # Define the condition to check for empty values for all keys except "Title" and "Description"
+    def is_empty_record(record):
+        keys_to_check = ['Ingredients', 'Instructions', 'Prep Time', 'Cook Time', 'Total Time', 'Cuisine', 'Course']
+        # Check if all keys except "Title" and "Description" have empty values
+        return all((key not in record or key in ['Title', 'Description'] or
+                    (isinstance(record[key], list) and len(record[key]) == 0) or
+                    (isinstance(record[key], str) and not record[key].strip())) for key in keys_to_check)
+
+    # Filter records based on the condition
+    empty_records = df.loc[df['receipe_data'].apply(is_empty_record)]
+
+    # Drop the rows with empty values for all specified keys
+    df.drop(empty_records.index, inplace=True)
+
+    # Convert the DataFrame to a list of dictionaries
+    updated_records = df['receipe_data'].tolist()
+    json_data = {
+            "code": 200,
+            "status": "Success",
+            "Message": "Successfully Fetched the Receipes List",
+            "receipe_data": updated_records
+        }
+    # Save the updated records to a new JSON file with original formatting
+    with open(r"C:\Users\venki\Work\scrapy_data_project_app\myproject\updated_final_recipes.json", 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, indent=4, ensure_ascii=False)
+
+    print("Updated JSON file saved to 'updated_final_recipes.json'.")
+else:
+    print("'receipe_data' column not found in the DataFrame.")
